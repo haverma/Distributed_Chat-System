498893a6b4fc282b3d44ef8b4cc58979c6f7630a
ron <redde@seas.upenn.edu>
2016-04-23 11:51:11 -0400

    removed verbose prints and extended encyption str

---
+++ Milestone3_Encryption/leader_election.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <map>
+#include <queue>
+#include <string>
+#include <list>
+#include "chat_system.h"
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <ctime>
+
+
+static bool deleteAll(sockaddr_in * element) 
+{ 
+    delete element; 
+    return true; 
+}
+
+void initiate_leader_election()
+{
+    struct sockaddr_in * psAddr;
+    int iSockFd = socket(AF_INET, SOCK_DGRAM, 0);
+    int iAddrLen = sizeof(sockaddr_in);
+    char acBuffer[BUFF_SIZE] = "";
+    bool bDeclareLeader;
+    msg_struct * psMsgStruct;
+
+    sprintf(&acBuffer[MSG_TYPE], "%d", (int) messageType::REQ_LEADER_ELECTION);
+    sprintf(&acBuffer[SENDER_LISTENING_PORT], "%d", iListeningPortNum);
+
+    clientListMutex.lock();
+
+    heartbeatMutex.lock();
+    declare_leader = true;
+    heartbeatMutex.unlock();
+    for (std::list<sockaddr_in *>::iterator i = lpsClients.begin(); i != lpsClients.end(); ++i)
+    {   std::cout << "Entry in lpsclients: " << ntohs((*i)->sin_port) << "\n";
+        //sendto(iSockFd, acBuffer, sizeof(acBuffer), 0, (struct sockaddr *) *i, sizeof(*(*i)));
+    }
+    for(std::list<msg_struct *>::iterator i = lpsClientInfo.begin(); i != lpsClientInfo.end(); ++i)
+    {
+        /* Ignore if the entry belongs to the current process */
+        if((*i)->name == username && (*i)->port == iListeningPortNum)
+            continue;
+
+        /* Create sockaddr struct and add it to lpsClients */
+        psAddr = new sockaddr_in;
+        psAddr->sin_family = AF_INET;
+        if(inet_pton(AF_INET, ((*i)->ipAddr).c_str(), &(psAddr->sin_addr)) <= 0)
+        {
+            fprintf(stderr, "Error while storing the IP address. Please retry\n");
+            exit(1);
+        }
+        psAddr->sin_port = htons((*i)->port);
+
+        lpsClients.push_back(psAddr);
+        
+        if((*i)->port > iListeningPortNum)
+        {
+            std::cout << "Sending REQ_LEADER_ELECTION\n";
+
+            sendto(iSockFd, acBuffer, BUFF_SIZE * sizeof(char), 0,
+                    (struct sockaddr *) psAddr, iAddrLen);
+        }
+    }
+    clientListMutex.unlock();
+
+    sleep(3);
+
+    heartbeatMutex.lock();
+    bDeclareLeader = declare_leader;
+    heartbeatMutex.unlock();
+
+    newLeaderElectedMutex.lock();
+    //std::cout << "leader_already_declared value = " << leader_already_declared << "\n";    
+    if(bDeclareLeader && !leader_already_declared)
+    {
+        /* Remove its entry from the client info list */
+        clientListMutex.lock();
+        for (std::list<msg_struct *>::iterator iterate = lpsClientInfo.begin(); iterate != lpsClientInfo.end(); ++iterate)
+        {
+            if((*iterate)->name == username && (*iterate)->port == iListeningPortNum)
+            {
+                delete *iterate;
+                lpsClientInfo.erase(iterate);
+                break;
+            }
+        }
+        for (std::list<msg_struct *>::iterator iterate = lpsClientInfo.begin(); iterate != lpsClientInfo.end(); ++iterate)
+        {
+            std::cout<< "Client Info: "<< (*iterate)->port << "\n";
+        }
+        clientListMutex.unlock();
+
+        /* Set sServerAddr */
+        if(inet_pton(AF_INET, sMyInfo.ipAddr.c_str(), &sServerAddr.sin_addr) <= 0)
+        {
+            fprintf(stderr, "Error while storing the IP address. Please retry\n");
+            exit(1);
+        }
+        sServerAddr.sin_port = htons(iListeningPortNum);
+
+        /* Set sServerInfo */
+        sServerInfo.name = username;
+        sServerInfo.ipAddr = sMyInfo.ipAddr;
+        sServerInfo.port = iListeningPortNum;
+
+        /* Copy the contents of sentBuffer to broadcast queue */
+        sentbufferMutex.lock();
+        int iTempSeqNum = 0;
+        for(std::map<int ,msg_struct *>::iterator it = sentBufferMap.begin();
+                it != sentBufferMap.end(); ++it)
+        {
+            psMsgStruct = it->second;
+            psMsgStruct->seqNum = iTempSeqNum++;
+            qpsBroadcastq.push(psMsgStruct);
+        }
+        sentBufferMap.clear();
+        sentbufferMutex.unlock();
+
+        /* Set is_server to true and send NEW_LEADER_ELECTED to all clients */
+        memset(acBuffer, 0x0, BUFF_SIZE * sizeof(char));
+        sprintf(&acBuffer[MSG_TYPE], "%d", (int) messageType::NEW_LEADER_ELECTED);
+        strcpy(&acBuffer[NAME], username.c_str());
+        strcpy(&acBuffer[DATA], sServerInfo.ipAddr.c_str());
+        sprintf(&acBuffer[SENDER_LISTENING_PORT], "%d", sServerInfo.port);
+
+        
+
+        clientListMutex.lock();
+        is_server = true;
+        iSeqNum = iMsgId = iExpSeqNum = 0;
+        for (std::list<sockaddr_in *>::iterator i = lpsClients.begin(); i != lpsClients.end(); ++i)
+        {   std::cout << "Sending new leader elected msg to" << ntohs((*i)->sin_port) << "\n";
+            sendto(iSockFd, acBuffer, sizeof(acBuffer), 0, (struct sockaddr *) *i, sizeof(*(*i)));
+        }
+        clientListMutex.unlock();
+    }
+    else
+    {
+        /* Clear the lpsClients list in case it it not a server */
+        /*for (std::list<sockaddr_in *>::iterator i = lpsClients.begin(); i != lpsClients.end(); ++i)
+        {
+            if(*i != NULL)
+            {
+                delete *i;
+            }
+            i = lpsClients.erase(i);
+        }*/
+        lpsClients.remove_if(deleteAll);
+
+
+         for (std::list<sockaddr_in *>::iterator i = lpsClients.begin(); i != lpsClients.end(); ++i)
+        {   
+            std::cout << "Entry in lpsclients after deletion: " << ntohs((*i)->sin_port) << "\n";
+        //sendto(iSockFd, acBuffer, sizeof(acBuffer), 0, (struct sockaddr *) *i, sizeof(*(*i)));
+        }
+    }
+    newLeaderElectedMutex.unlock();
+}
---
+++ Milestone3_Encryption/user_listener.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <map>
+#include <queue>
+#include "./chat_system.h"
+#include <string.h>
+#include <string>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include "encoder.hpp"
+
+void user_listener()
+{
+    char acBuffer[BUFF_SIZE] = "";
+    int iTemp = 0;
+    msg_struct * psMsg = NULL;
+    int iSocketFd;
+
+    iSocketFd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if (iSocketFd < 0)
+    {
+        fprintf(stderr, "Error while opening socket\n");
+        exit(1);
+    }
+
+    while(true)
+    {
+        
+        strcpy(acBuffer, "");
+        iTemp = 0;
+        /* Fetch the user input */
+        if (fgets(&acBuffer[DATA], BUFF_SIZE - DATA - 1, stdin) == NULL)
+        {
+            if(!is_server)
+            {
+                /* Store CHAT msg into acBuffer and send it to the server */
+                sprintf(&acBuffer[MSG_TYPE], "%d", (int) messageType::CLIENT_EXITED);
+                sprintf(&acBuffer[DATA], "%d", iListeningPortNum);
+                sendto(iSocketFd, acBuffer, BUFF_SIZE, 0,
+                (struct sockaddr *) &sServerAddr, sizeof(sockaddr_in));
+                exit(1);
+            }
+            else
+            {
+                std::cout<<"Exiting the chat application... Server Closing.. !!"<<"\n";
+                exit(1);
+            }
+                
+        }
+        
+        iTemp = strlen(&acBuffer[DATA]);
+        acBuffer[DATA + iTemp - 1] = '\0';
+        if(!strcmp(&acBuffer[DATA], ""))
+            continue;
+        
+
+        char* encryption = new char[BUFF_SIZE-DATA];
+        ENCODER::encrypt(&acBuffer[DATA], BUFF_SIZE-DATA, "TEAM BLOOPERS ROCKS YOUR SOCK TO THE MOON AND BACK!", &encryption);
+        for (int i=DATA; i<BUFF_SIZE; i++)
+            acBuffer[i] = encryption[i-DATA];
+        delete [] encryption;
+        
+        if(is_server)
+        {
+            /* Create msg by filling the received msg into a struct and push
+             * it to the broadcast queue */
+            //psMsg = (msg_struct *) malloc(sizeof(msg_struct));
+            psMsg = new msg_struct;//();
+            if(psMsg == NULL)
+            {
+                fprintf(stderr, "Malloc failed. Please retry\n");
+                continue;
+            }
+            psMsg->msgType = messageType::MSG;
+            seqNumMutex.lock();
+            psMsg->seqNum = iSeqNum;
+            iSeqNum++;
+            seqNumMutex.unlock();
+            psMsg->name = username;
+            psMsg->data = &acBuffer[DATA];
+            broadcastMutex.lock();
+            qpsBroadcastq.push(psMsg);
+            broadcastMutex.unlock();
+        }
+        else
+        {
+            /* Store CHAT msg into acBuffer and send it to the server */
+            sprintf(&acBuffer[MSG_TYPE], "%d", (int) messageType::CHAT);
+            strcpy(&acBuffer[NAME], username.c_str());
+            sprintf(&acBuffer[MSG_ID], "%d", iMsgId);
+            sprintf(&acBuffer[SENDER_LISTENING_PORT], "%d", iListeningPortNum);
+            sendto(iSocketFd, acBuffer, BUFF_SIZE, 0,
+                    (struct sockaddr *) &sServerAddr, sizeof(sockaddr_in));
+
+            /* Add the message to sent buffer */
+            psMsg = new msg_struct;
+            psMsg->msgType = messageType::CHAT;
+            psMsg->name = username;
+            psMsg->data = &acBuffer[DATA];
+            psMsg->msgId = iMsgId;
+            psMsg->timestamp = time(NULL);
+            psMsg->attempts = 1;
+            sentbufferMutex.lock();
+
+            /* Logging */
+            /*
+            std::cout << "During sending: Current msg ID sending: " << iMsgId << "\n\n";
+            for(auto it = sentBufferMap.cbegin(); it != sentBufferMap.cend(); ++it)
+            {
+                std::cout << it->first << " " << it->second << "\n";
+            }
+            */
+
+            sentBufferMap[iMsgId] = psMsg;
+            sentbufferMutex.unlock();
+            iMsgId++;
+        }
+
+    }
+}
