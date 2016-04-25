bbd6ea71af1d3602af180b1a9263849c6b1e3865
Harsh Verma <hverma@seas.upenn.edu>
2016-04-24 23:15:07 -0400

    Fair Q

---
+++ Milestone3_FairQ/broadcast_message.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <map>
+#include <queue>
+#include <string>
+#include <list>
+#include "chat_system.h"
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+void broadcast_message();
+bool trim_broadcast_message(std::map<int, msg_struct *> broadcastbuffer);
+std::string collect_clients_info();
+
+void broadcast_message(){
+
+   
+	   while(1)
+	   {	
+	   	   if(is_server)
+	   	   {   
+		       std::map<int, std::queue<msg_struct *>>::iterator queues_itereator =broadcastPortToQueueMap.begin();
+		   		char buf[BUFF_SIZE];
+		   	    for (queues_itereator; queues_itereator!=broadcastPortToQueueMap.end(); ++queues_itereator)
+		   	    {
+		   	        
+			   	    	std::queue<msg_struct *> current_client_queue = queues_itereator->second;
+			   	        if(!queues_itereator->second.empty())
+			   	        {
+				   	    	/* Iterating through the queues of all the clients*/
+				   	    	msg_struct* psMsgStruc = queues_itereator->second.front();
+				   	    	queues_itereator->second.pop();
+
+				   	    	if(psMsgStruc->msgType == messageType::MSG)
+					   	    {	
+					   	    	seqNumMutex.lock();
+	                    		psMsgStruc->seqNum = iSeqNum;
+	                    		iSeqNum++;
+	                    		seqNumMutex.unlock();
+	                    	}
+
+
+				   	    	broadcastbufferMutex.lock();
+				            //mlsBroadcastm.insert ( std::pair<long,msg_struct *>(psMsgStruc->seqNum, psMsgStruc) );
+				            if(broadcastBufferMap.size() == BBMAP_THRESHOLD)
+				            {
+				                trim_broadcast_message(broadcastBufferMap);
+				            }
+
+				            broadcastBufferMap[psMsgStruc->seqNum] = psMsgStruc;
+				            broadcastbufferMutex.unlock();
+				            std::string message;
+				            memset((char *)&buf, 0, sizeof(buf));
+				            if(psMsgStruc->msgType == CLIENT_LIST)
+				            {
+				                message = collect_clients_info();
+				                sprintf(&buf[MSG_TYPE], "%d", psMsgStruc->msgType);
+				            	sprintf(&buf[DATA], "%s", message.c_str());
+				            }
+				            else if(psMsgStruc->msgType == NEW_CLIENT_INFO)
+				            {
+				            	message = psMsgStruc->data;
+				                sprintf(&buf[MSG_TYPE], "%d", psMsgStruc->msgType);
+				            	sprintf(&buf[DATA], "%s", message.c_str());
+				            }
+				            else
+				            {
+				            	message = psMsgStruc->data;
+				            	sprintf(&buf[MSG_TYPE], "%d", psMsgStruc->msgType);
+				            	sprintf(&buf[DATA], "%s", message.c_str());
+					            sprintf(&buf[NAME], "%s", psMsgStruc->name.c_str());
+					            sprintf(&buf[SEQ_NUM], "%d", psMsgStruc->seqNum);
+				            }
+				            int sockfd = socket(PF_INET, SOCK_DGRAM, 0);	
+				            int n = sendto(sockfd, buf, sizeof(buf), 0, (struct sockaddr *)&sServerAddr, sizeof(sServerAddr));
+					        if (n < 0) 
+					            std::cout<< "Error in sending to server \n";
+					        for (std::list<sockaddr_in *>::iterator i = lpsClients.begin(); i != lpsClients.end(); ++i) 
+					        {
+					            n = sendto(sockfd, buf, sizeof(buf), 0, (struct sockaddr *)*i, sizeof(*(*i)));
+					            if (n < 0) 
+					                std::cout<<"Error in sending to client\n"; 
+					        }
+					        close(sockfd);
+
+				    	}
+				    	else
+				    	{
+				    		continue;
+				    	}
+				    	 sleep(9); //This sleep can be used to test the feature of round robin scheduling
+
+			    }
+			     
+			    //sleep(1);
+		    }
+	    
+    }
+}
+
+bool trim_broadcast_message(std::map<int, msg_struct *> broadcastbuffer)
+{
+	std::map<int,msg_struct *>::iterator it;
+
+	//Taking the first element from the broadcast buffer
+    it = broadcastbuffer.begin();
+    broadcastbufferMutex.lock();
+    msg_struct* temp = it-> second;
+    
+
+    // Deleting the element and freeing the pointer to struc
+	if(temp != NULL)
+	{
+		if(temp -> addr != NULL)
+		{
+			//Freeing the address structure contained 
+			//by the addr structure
+			free(temp -> addr);
+			temp -> addr = NULL;
+		}
+	}
+	free(temp);
+	temp = NULL;
+    broadcastbuffer.erase(it);
+    broadcastbufferMutex.unlock();
+
+	return true;
+
+}
+
+std::string collect_clients_info()
+{
+	std::string message = "";
+	for (std::list<msg_struct *>::iterator i = lpsClientInfo.begin(); i != lpsClientInfo.end(); ++i)
+	{
+        message = message + (*i)->name + ":" + (*i)->ipAddr + ":" + std::to_string((*i)->port) + "\n";
+	}
+	return message;
+}
---
+++ Milestone3_FairQ/heartbeat.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <map>
+#include <queue>
+#include <string>
+#include <list>
+#include "chat_system.h"
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+void heartbeat();
+void flush_dead_clients(std::list<int> deadclients);
+void initiate_leader_election();
+
+void heartbeat()
+{
+    char buf[BUFF_SIZE];
+    int n;
+    int sockfd = socket(PF_INET, SOCK_DGRAM, 0);
+    bool bListEmpty, bServerAlive;
+    while(1)
+    {
+        /* If server, check if clients are alive */
+        if(is_server)
+        {
+            heartbeatMutex.lock();
+            bListEmpty = liCurrentClientPort.empty();
+            heartbeatMutex.unlock();
+
+            if(!bListEmpty)
+            {
+                flush_dead_clients(liCurrentClientPort);
+                heartbeatMutex.lock();
+                liCurrentClientPort.clear();
+                heartbeatMutex.unlock();
+            }
+            
+            for (std::list<sockaddr_in *>::iterator i = lpsClients.begin(); i != lpsClients.end(); ++i)
+            {
+                liCurrentClientPort.push_back(ntohs((*i)->sin_port));
+            }
+
+            sprintf(&buf[MSG_TYPE], "%d", messageType::CLIENT_HEARTBEAT);
+            for (std::list<sockaddr_in *>::iterator i = lpsClients.begin(); i != lpsClients.end(); ++i)
+            {
+                n = sendto(sockfd, buf, sizeof(buf), 0, (struct sockaddr *) *i, sizeof(*(*i)));
+                if (n < 0)
+                {
+                    fprintf(stdout, "Error while sending client heartbeat msg\n");
+                }
+            }
+            sleep(3);
+        }
+        /* If client, check if server is alive */
+        else
+        {
+            sprintf(&buf[MSG_TYPE], "%d", messageType::SERVER_HEARTBEAT);
+            sprintf(&buf[SENDER_LISTENING_PORT], "%d", iListeningPortNum);
+            heartbeatMutex.lock();
+            is_server_alive = false;
+            heartbeatMutex.unlock();
+            //std::cout << "server addr in heartbeat: " << ntohs(sServerAddr.sin_port) << "\n";
+            n = sendto(sockfd, buf, sizeof(buf), 0, (struct sockaddr *) &sServerAddr, sizeof(sServerAddr));
+            if (n < 0)
+            {
+                fprintf(stdout, "Error while sending server heartbeat msg\n");
+            }
+            sleep(3);
+            heartbeatMutex.lock();
+            bServerAlive = is_server_alive;
+            heartbeatMutex.unlock();
+            if(!bServerAlive)
+            {
+                heartbeatMutex.lock();
+                if(0 == iResponseCount)
+                {
+                    iResponseCount++;
+                    heartbeatMutex.unlock();
+                }
+                else
+                {
+                    iResponseCount = 0;
+                    heartbeatMutex.unlock();
+                    newLeaderElectedMutex.lock();
+                    //std::cout << "setting leader_already_declared to false\n";
+                    leader_already_declared = false;
+                    newLeaderElectedMutex.unlock();
+                    initiate_leader_election();
+                    sleep(3);
+                }
+            }
+        }
+    }
+    close(sockfd);
+}
+
+void flush_dead_clients(std::list<int> deadclients)
+{
+    std::list<int>::iterator i = deadclients.begin();
+    while(true)
+    {
+        heartbeatMutex.lock();
+        if(i == deadclients.end())
+        {
+            heartbeatMutex.unlock();
+            break;
+        }
+        int port = (*i);
+        heartbeatMutex.unlock();
+
+        std::list<msg_struct *>::iterator iter = lpsClientInfo.begin();
+
+        /* Remove entry from lpsClientInfo */
+        while(true)
+        {
+            clientListMutex.lock();
+            if(iter == lpsClientInfo.end())
+            {
+                clientListMutex.unlock();
+                break;
+            }
+            msg_struct * psClientInfo = *iter; 
+            clientListMutex.unlock();
+
+            if(psClientInfo->port == port)
+            {
+                msg_struct * psMsg = new msg_struct;//();
+                char acTempStr[100] = "\0";
+                psMsg->msgType = messageType::NEW_CLIENT_INFO;
+                sprintf(acTempStr, "NOTICE: %s left the chat or crashed",(psClientInfo->name).c_str());
+                psMsg->data = acTempStr;
+                broadcastMutex.lock();
+    broadcastPortToQueueMap[iListeningPortNum].push(psMsg);
+    broadcastMutex.unlock();
+        
+                delete psClientInfo;
+                clientListMutex.lock();
+                iter = lpsClientInfo.erase(iter);
+                clientListMutex.unlock();
+            }
+            iter++;
+        }
+
+        std::list<sockaddr_in *>::iterator iter1 = lpsClients.begin();
+
+        /* Remove entry from lpsClients */
+        while(true)
+        {
+            clientListMutex.lock();
+            if(iter1 == lpsClients.end())
+            {
+                clientListMutex.unlock();
+                break;
+            }
+            int currPort = ntohs((*iter1)->sin_port); 
+            clientListMutex.unlock();
+            
+            if(currPort == port)
+            {
+                clientListMutex.lock();
+                delete *iter1;
+                iter1 = lpsClients.erase(iter1);
+                clientListMutex.unlock();
+            }
+            iter1++;
+        }
+        i++;
+    }
+    msg_struct * temp = new msg_struct;
+    temp->msgType = messageType::CLIENT_LIST;
+     broadcastMutex.lock();
+    broadcastPortToQueueMap[iListeningPortNum].push(temp);
+    broadcastMutex.unlock();
+        
+}
